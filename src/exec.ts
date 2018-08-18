4226e3bacb74199d7731da6a4918892775eed25e
Steve King <steve@mydev.co>
2018-08-18 08:37:25 +0100

    Switch to yarn for dependency loading and to TypeScript for the source

---
+++ src/exec.ts
@@ -0,0 +1,33 @@
+
+import {spawn} from 'child_process';
+
+/**
+ * Spawns a child process to run the specified command. When there is a non-zero exit code, and there has been
+ * output to `stderr` the resulting promise will throw and must be caught by the caller.
+ *
+ * @param {string} command
+ * @param {string...} params
+ * @returns {Promise<string>}
+ */
+export function execP (command: string, ...params: string[]): Promise<string> {
+   return new Promise((ok, fail) => {
+
+      const out: Buffer[] = [];
+      const err: Buffer[] = [];
+      const spawned = spawn(command, params, {});
+
+      spawned.stdout.on('data', data => out.push(data));
+      spawned.stderr.on('data', data => err.push(data));
+
+      spawned.on('error', e => err.push(new Buffer(String(e.stack), 'ascii')));
+
+      spawned.on('exit', (exitCode, exitSignal) => {
+         if (exitCode && err.length) {
+            return fail(new Error(Buffer.concat(err).toString('utf-8')));
+         }
+
+         ok(Buffer.concat(out).toString('utf-8'));
+      });
+
+   });
+}
